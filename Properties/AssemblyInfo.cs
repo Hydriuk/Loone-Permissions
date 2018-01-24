95d39e4f8ab8a4cda8b8e3fd6a415b951ccfcd39
ChubbyQuokka <graybad1@icloud.com>
2018-01-23 19:50:39 -0500

    Updated Assembly Copyright and Made Build Output Pretty

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -3,6 +3,6 @@
 [assembly: AssemblyTitle("LoonePermissions.dll")]
 [assembly: AssemblyCompany("52 Piece Games")]
 [assembly: AssemblyProduct("MySql Permission Provider")]
-[assembly: AssemblyCopyright("(c) 2017 52 Piece Games")]
+[assembly: AssemblyCopyright("(c) 2018 52 Piece Games")]
 
-[assembly: AssemblyVersion("1.0.2.0")]
+[assembly: AssemblyVersion("1.0.2.1")]
